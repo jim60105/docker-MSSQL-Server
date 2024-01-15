e680fc0c0c6370ccec8251fe9eba96b628106a9b
陳鈞 <jim60105@gmail.com>
2024-01-16 02:07:57 +0800

    chore: refactor volume-backup Dockerfile
    - Introduce a Hadolint configuration to ignore specific linting rules for Dockerfiles
    - Switch base image in volume-backup Dockerfile from `loomchild/volume-backup` to `alpine:3`
    - Install additional tools `xz`, `tar`, `pigz`, and `zstd` in volume-backup Dockerfile
    - Download the `volume-backup.sh` script directly from its repository instead of copying from local build context
    - Set file ownership and permissions during COPY in volume-backup Dockerfile
    - Define Docker image to run as root with `USER 0`
    - Add explicit `VOLUME` statements for `/volume` and `/backup` in volume-backup Dockerfile
    - Modify `backup.sh` script to handle non-existent `/backup` directory more gracefully and fix date logging format
    - Modify `restore.sh` script to handle non-existent `/backup` directory more gracefully
    - Refactor scripts to use `volume-backup.sh` from the script folder instead of an absolute path
    
    Signed-off-by: 陳鈞 <jim60105@gmail.com>

--- volume-backup/script/restore.sh
+++ volume-backup/script/restore.sh
@@ -1,22 +1,21 @@
 #!/bin/bash
 date +"%F %T"
 
-scriptFolder="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+scriptFolder="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 
 # Move to backup folder
 mkdir -p /backup
-cd /backup
+cd /backup || exit
 
-source ${scriptFolder}/stopContainer.sh
+source "${scriptFolder}"/stopContainer.sh
 
 V=$(su - root -c "docker volume ls -f 'label=${BACKUP_LABEL}' -q")
-for i in ${V}
-do
+for i in ${V}; do
     echo "Restore ${i}..."
 
     # volume-backup.sh from base image
     # https://github.com/loomchild/volume-backup/blob/master/volume-backup.sh
-    su - root -c "/volume-backup.sh restore -f -c gz ${i}"
+    su - root -c "${scriptFolder}/volume-backup.sh restore -f -c gz ${i}"
 done
 
-source ${scriptFolder}/startContainer.sh
+source "${scriptFolder}"/startContainer.sh
